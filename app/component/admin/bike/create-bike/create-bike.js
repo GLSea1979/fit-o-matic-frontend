0236bad27973da5cc7ff7c402b3a0f11d83d2112
GLSea1979 <builtbygl@gmail.com>
2017-04-17 08:46:03 -0700

    bike service

---
+++ app/component/admin/bike/create-bike/create-bike.js
@@ -0,0 +1,29 @@
+'use strict';
+
+require('./_create-bike.scss');
+
+module.exports = {
+  template: require('./create-bike.html'),
+  controller: ['$log', 'bikeService', CreateBikeController],
+  controllerAs: 'createBikeCtrl',
+  bindings: {
+    bike: '<'
+  }
+};
+
+function CreateBikeController($log, bikeService) {
+  $log.debug('CreateBikeController');
+
+  this.bike = {};
+
+  this.createBike = function() {
+    $log.debug('createBikeCtrl.createBike');
+    bikeService.createBike(this.bike)
+    .then( () => {
+      this.bike.name = null;
+      this.bike.category = null;
+      this.bike.price = null;
+      this.bike.modelYear = null;
+    });
+  }
+}
