41643c20f6826dd9d747484fea5146dc40fd1545
Zachary Crumbo <zcrumbo@mac.com>
2017-04-19 13:17:35 -0700

    added modal scaffold

---
+++ app/component/bikes/detail-modal/detail-modal.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('./_detail-modal.scss');
+
+module.exports = {
+  template: require('./detail-modal.html'),
+  controller: ['$log',   DetailModalController],
+  controllerAs: 'detailModalCtrl',
+  bindings: {
+    resolve: '<',
+  }
+};
+
+function DetailModalController($log){
+  $log.debug('DetailModalController');
+
+}
