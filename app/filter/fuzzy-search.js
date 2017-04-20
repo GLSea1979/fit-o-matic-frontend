3800f5d7fc9e396e16926e98b5af302a1a3502c6
reganoneill <rkrotho@gmail.com>
2017-04-20 13:17:53 -0700

    bike grid styling

--- app/filter/fuzzy-search.js
+++ app/filter/fuzzy-search.js
@@ -1,25 +1,5 @@
 'use strict';
 
-//  TODO : fuzzy search needs to be linked to html with a ng-repeat, Brian did fuzzy search during day 32-filters and directives. It will be awesome.
-
-// module.exports = function() {
-
-//   return function(bikes, searchTerm) {
-//     console.log(bikes, searchTerm);
-//     let fuzzyRegex = generateFuzzyRegex(searchTerm);
-
-//     return bikes.filter( bike => {
-//       return fuzzyRegex.test(bike.name.toUpperCase());
-//     });
-//   };
-// };
-
-// function generateFuzzyRegex(input) {
-//   if(!input) return /.*/;
-//   let fuzzyString = '.*' + input.toUpperCase().split('').join('.*') + '.*';
-//   return new RexExp(fuzzyString);
-// };
-
 module.exports = function() {
   return function(bikes, searchTerm){
     let fuzzyRegex = generateFuzzyString(searchTerm);
