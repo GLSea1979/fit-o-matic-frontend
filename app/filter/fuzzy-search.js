7b58d619287f404be1f404e47f4f0a07991ac4f1
Zachary Crumbo <zcrumbo@mac.com>
2017-04-20 11:11:47 -0700

    sorry guys

--- app/filter/fuzzy-search.js
+++ app/filter/fuzzy-search.js
@@ -2,20 +2,37 @@
 
 //  TODO : fuzzy search needs to be linked to html with a ng-repeat, Brian did fuzzy search during day 32-filters and directives. It will be awesome.
 
+// module.exports = function() {
+
+//   return function(bikes, searchTerm) {
+//     console.log(bikes, searchTerm);
+//     let fuzzyRegex = generateFuzzyRegex(searchTerm);
+
+//     return bikes.filter( bike => {
+//       return fuzzyRegex.test(bike.name.toUpperCase());
+//     });
+//   };
+// };
+
+// function generateFuzzyRegex(input) {
+//   if(!input) return /.*/;
+//   let fuzzyString = '.*' + input.toUpperCase().split('').join('.*') + '.*';
+//   return new RexExp(fuzzyString);
+// };
+
 module.exports = function() {
+  return function(bikes, searchTerm){
+    let fuzzyRegex = generateFuzzyString(searchTerm);
 
-  return function(bikes, searchTerm) {
-    console.log(bikes, searchTerm, 'ppppppppppppppppp');
-    let fuzzyRegex = generateFuzzyRegex(searchTerm);
-
-    return bikes.filter( bike => {
-      return fuzzyRegex.test(bike.name.toUpperCase());
+    return bikes.filter(bike => {
+      return fuzzyRegex.test(bike.bikeName.toUpperCase());
     });
   };
 };
 
-function generateFuzzyRegex(input) {
-  if(!input) return /.*/;
-  let fuzzyString = '.*' + input.toUpperCase().split('').join('.*') + '.*';
-  return new RexExp(fuzzyString);
-};
+function generateFuzzyString(input) {
+  if (!input) return /.*/;
+  let fuzzyString = '.*' + input.toUpperCase().split('').join('') + '.*';
+  console.log(fuzzyString);
+  return new RegExp(fuzzyString);
+}
