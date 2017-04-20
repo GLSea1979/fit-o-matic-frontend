617f7c7d7e6804c2293bf2c09cd378f7ce59775b
GLSea1979 <builtbygl@gmail.com>
2017-04-19 19:43:33 -0700

    added logo and content

--- app/component/profile/profile-instructions/profile-instructions.js
+++ app/component/profile/profile-instructions/profile-instructions.js
@@ -11,7 +11,7 @@ module.exports = {
   }
 };
 
-function ProfileResultsController($log, profileService){
+function ProfileInstructionsController($log, profileService){
   $log.debug('ProfileInstructionsController');
 
 
